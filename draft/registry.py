be594a268a17a36b9c8c6f09c43217b32f2be337
fmsunyh <fmsunyh@gmail.com>
2019-12-31 14:58:31 +0800

    in order to debug mmdet, Set num_workers = 0

---
+++ draft/registry.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+
+"""
+Copyright 2018-2019  Firmin.Sun (fmsunyh@gmail.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+# -----------------------------------------------------
+# @Time    : 12/30/2019 3:55 PM
+# @Author  : Firmin.Sun (fmsunyh@gmail.com)
+# @Software: ZJ_AI
+# -----------------------------------------------------
+# -*- coding: utf-8 -*-
+import inspect
+
+
+class Registry(object):
+
+    def __init__(self, name):
+        self._name = name
+        self._module_dict = dict()
+
+    def __repr__(self):
+        format_str = self.__class__.__name__ + '(name={}, items={})'.format(
+            self._name, list(self._module_dict.keys()))
+        return format_str
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def module_dict(self):
+        return self._module_dict
+
+    def get(self, key):
+        return self._module_dict.get(key, None)
+
+    def _register_module(self, module_class):
+        """Register a module.
+
+        Args:
+            module (:obj:`nn.Module`): Module to be registered.
+        """
+        if not inspect.isclass(module_class):
+            raise TypeError('module must be a class, but got {}'.format(
+                type(module_class)))
+        module_name = module_class.__name__
+        if module_name in self._module_dict:
+            raise KeyError('{} is already registered in {}'.format(
+                module_name, self.name))
+        self._module_dict[module_name] = module_class
+
+    def register_module(self, cls):
+        self._register_module(cls)
+        return cls
+
+
+ANIMAL = Registry('animal')
+
+# @ANIMAL.register_module
+class Dog(object):
+    def __init__(self):
+        pass
+
+    def run(self):
+        print('running dog')
+
+# ANIMAL.register_module(Dog)
+
+
+dog = ANIMAL.get('Dog')
+
+d = dog()
+d.run()
